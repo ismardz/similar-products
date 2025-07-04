6435094776c53959de499e679c962f8e5cc7d03c
Ismael Rodríguez <ismael.rr@globant.com>
2025-07-04 16:42:55 +0200

    ADDED * Initial commit

---
+++ src/test/java/com/globant/similarproducts/SimilarProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.similarproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
