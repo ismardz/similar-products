f126fe8ae5c3c19571047a8d323185063da68200
Ismael Rodríguez <ismael.rr@globant.com>
2025-07-04 16:55:01 +0200

    ADDED * proposal files

---
+++ proposal/generated/src/main/java/org/openapitools/api/ProductApiController.java
@@ -0,0 +1,48 @@
+package org.openapitools.api;
+
+import org.openapitools.model.ProductDetail;
+import java.util.Set;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.lang.Nullable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-07-04T16:24:56.388980+02:00[Europe/Madrid]", comments = "Generator version: 7.14.0")
+@Controller
+@RequestMapping("${openapi.similarProducts.base-path:}")
+public class ProductApiController implements ProductApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public ProductApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
